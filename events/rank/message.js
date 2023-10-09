5e4725c331135a24e79e75085392389e12090e41
Daniel <dmunier27@gmail.com>
2023-10-09 11:22:01 -0300

    new: ranking message

--- events/rank/message.js
+++ events/rank/message.js
@@ -3,13 +3,24 @@ const {
 } = require("discord.js")
 
 const fs = require("fs")
-
+const { user_db, guild_db } = require("../../utils/guild")
 
 module.exports = {
     name: Events.MessageCreate,
     async execute(message){
-        console.log(message.author.username)
+        if(message.author.bot) return;
+        let guild =  guild_db.read(message.guildId)
+        if(!guild){
+            guild_db.create(message.guildId)
+        }
+        let user = user_db.read(message.guildId, message.author.id)
+        if(!user) {
+            user_db.create(message.guildId, message.author.id)
+        }
 
+        user_db.update(message.guildId, message.author.id, 0, 1)
+
+        
             
     }
 }
