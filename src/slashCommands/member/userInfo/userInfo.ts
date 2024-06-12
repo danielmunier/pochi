32c29752f2efe7e15040b71907c14da3842d5d57
Daniel <dmunier27@gmail.com>
2024-06-12 02:44:03 -0300

    fix userInfo

--- src/slashCommands/member/userInfo/userInfo.ts
+++ src/slashCommands/member/userInfo/userInfo.ts
@@ -1,29 +1,34 @@
-import { SlashCommandBuilder, AttachmentBuilder, PermissionFlagsBits } from "discord.js"
+import { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, SlashCommandUserOption } from "discord.js"
 import { SlashCommand } from "../../../types";
-import logger from "../../../util/beautyLog";
-import axios from "axios"
-
 
 
 
 const command : SlashCommand = {
-    command: new SlashCommandBuilder()
+     command: new SlashCommandBuilder()
     .setName("userinfo")
-    .setDescription("Shows the bang of Makima character")
-
-    ,
+    .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
+    .addUserOption(option =>
+      option.setName("user").setDescription("Select a user (optional)")
+    ).setDescription("Get information about a user on this server."),
     execute: async interaction => {
-        try {
-            
 
-        const attachment = new AttachmentBuilder("./assets/makimaBang.jpg")
-        logger.info(`${interaction.client.ws.ping}`)
-        interaction.reply({
-            files: [attachment]
-        })
-        } catch(e) {
-            logger.error(`${e}`)
-        }
+        const user = interaction.options.getUser("user") || interaction.user
+        const member = await interaction.guild?.members.fetch(user.id) 
+        if(!member) return interaction.reply("Member not found")
+        const icon = user.displayAvatarURL()
+        const tag = user.tag
+
+        const embed = new EmbedBuilder()
+        .setColor("Blue")
+        .setAuthor({name: "Pochi"})
+        .setThumbnail(icon)
+        .addFields({name: "Member", value: `${user}`, inline:false})
+        .addFields({name: "Roles", value: `${member?.roles.cache.map(role => role).join(' ')}`, inline: true})
+        .addFields({name: "Joined Discord", value: `${user.createdAt.toLocaleString()}`, inline:true})
+        .addFields({name: "Joined Server", value: `${member?.joinedAt?.toLocaleString()}`, inline:true})
+        .setFooter({text: tag})
+        interaction.reply({embeds: [embed]})
+        
     },
     cooldown: 10
 }
